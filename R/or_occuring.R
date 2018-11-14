6e1ff6ae11afa31a9d009c6b28997941a991c213
James Laird-Smith <jameslairdsmith@gmail.com>
2018-11-14 20:49:32 +0000

    made changes to or_occuring() function.

--- R/or_occuring.R
+++ R/or_occuring.R
@@ -1,26 +1,44 @@
 or_occuring <- function(object, ...)
   UseMethod("or_occuring")
 
-or_occuring.on_element <- function(elem_1, elem_2){
+or_occuring.default <- function(elem_1, elem_2){
+
+  if(!(inherits(elem_1, "date_element") | inherits(elem_1, "schedule"))){
+    stop("both arguments must be either of type 'date_element' or 'schedule'", call. = F)
+  }
+
+  if(!(inherits(elem_2, "date_element") | inherits(elem_2, "schedule"))){
+    stop("both arguments must be either of type 'date_element' or 'schedule'", call. = F)
+  }
 
   out <- list(elem_1, elem_2)
 
-  class(out) <- "or_schedule"
+  class(out) <- c("schedule","or_schedule")
 
   out
+
 }
 
-or_occuring.or_schedule <- function(or_schedule, elem){
+#or_occuring.on_element <- function(elem_1, elem_2){
 
-  out <- list(
-    or_schedule,
-    elem
-  )
+  #out <- list(elem_1, elem_2)
+
+  #class(out) <- "or_schedule"
+
+  #out
+#}
+
+#or_occuring.or_schedule <- function(or_schedule, elem){
+
+ # out <- list(
+#    or_schedule,
+#    elem
+#  )
 
   #or_schedule[[length(or_schedule) + 1]] <- elem
 
-  class(out) <-  "or_schedule"
+#  class(out) <-  "or_schedule"
 
-  out
+#  out
 
-}
+#}
