a2f6d727c6bc31fdf434a238cf59c86150c527e2
James Laird-Smith <jameslairdsmith@gmail.com>
2018-11-15 11:52:17 +0000

    deleted old code.

--- R/or_occuring.R
+++ R/or_occuring.R
@@ -18,27 +18,3 @@ or_occuring.default <- function(elem_1, elem_2){
   out
 
 }
-
-#or_occuring.on_element <- function(elem_1, elem_2){
-
-  #out <- list(elem_1, elem_2)
-
-  #class(out) <- "or_schedule"
-
-  #out
-#}
-
-#or_occuring.or_schedule <- function(or_schedule, elem){
-
- # out <- list(
-#    or_schedule,
-#    elem
-#  )
-
-  #or_schedule[[length(or_schedule) + 1]] <- elem
-
-#  class(out) <-  "or_schedule"
-
-#  out
-
-#}
