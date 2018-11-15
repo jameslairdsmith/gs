5f5806d99e2bcaf6476450c2dd17950ec1806fd3
James Laird-Smith <jameslairdsmith@gmail.com>
2018-11-15 16:45:20 +0000

    added the both_logical() function.

--- R/misc.R
+++ R/misc.R
@@ -9,3 +9,16 @@ is_schedule <- function(x){
 is_temporal <- function(x){
   is_date_element(x) | is_schedule(x)
 }
+
+both_logical <- function(x){
+
+  if(!is_schedule(x)){
+    stop("Not a schedule object", call. = F)
+  }
+
+  if(length(x) != 2){
+    stop("Not legitimate", call. = F)
+  }
+
+  is.logical(x[[1]]) & is.logical(x[[2]])
+}
