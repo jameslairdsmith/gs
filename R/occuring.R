7d0d8fafb2a91574e0f4ac4e38770ea4227ded35
James Laird-Smith <jameslairdsmith@gmail.com>
2018-11-15 12:34:51 +0000

    added the and_occuring() function.

--- R/occuring.R
+++ R/occuring.R
@@ -14,3 +14,20 @@ or_occuring <- function(elem_1, elem_2){
 
   out
 }
+
+and_occuring <- function(elem_1, elem_2){
+
+  if(!is_temporal(elem_1)){
+    stop("both arguments must be either of type 'date_element' or 'schedule'", call. = F)
+  }
+
+  if(!is_temporal(elem_2)){
+    stop("both arguments must be either of type 'date_element' or 'schedule'", call. = F)
+  }
+
+  out <- list(elem_1, elem_2)
+
+  class(out) <- c("schedule", "and_schedule")
+
+  out
+}
