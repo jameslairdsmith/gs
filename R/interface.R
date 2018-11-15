e8f9639ef2b4d3af0a601fd16ada4f4d1f65d5f6
James Laird-Smith <jameslairdsmith@gmail.com>
2018-11-15 12:22:00 +0000

    changed function name in scope.

--- R/interface.R
+++ R/interface.R
@@ -1,52 +1,52 @@
 on_quarter <- function(x, ...){
-  occuring_on(x, lubridate::quarter, ...)
+  make_element(x, lubridate::quarter, ...)
 }
 
 on_semester <- function(x, ...){
-  occuring_on(x, lubridate::semester, ...)
+  make_element(x, lubridate::semester, ...)
 }
 
 on_year <- function(x, ...){
-  occuring_on(x, lubridate::year, ...)
+  make_element(x, lubridate::year, ...)
 }
 
 on_isoyear <- function(x, ...){
-  occuring_on(x, lubridate::isoyear, ...)
+  make_element(x, lubridate::isoyear, ...)
 }
 
 on_epiyear <- function(x, ...){
-  occuring_on(x, lubridate::epiyear, ...)
+  make_element(x, lubridate::epiyear, ...)
 }
 
 on_month <- function(x, label = TRUE, abbr = TRUE, ...){
-  occuring_on(x, lubridate::month, label = label, abbr = abbr, ...)
+  make_element(x, lubridate::month, label = label, abbr = abbr, ...)
 }
 
 on_wday <- function(x, label = TRUE, abbr = TRUE, ...){
-  occuring_on(x, lubridate::wday, label = label, abbr = abbr, ...)
+  make_element(x, lubridate::wday, label = label, abbr = abbr, ...)
 }
 
 on_mday <- function(x, ...){
-  occuring_on(x, lubridate::mday, ...)
+  make_element(x, lubridate::mday, ...)
 }
 
 on_yday <- function(x, ...){
-  occuring_on(x, lubridate::yday, ...)
+  make_element(x, lubridate::yday, ...)
 }
 
 on_qday <- function(x, ...){
-  occuring_on(x, lubridate::qday, ...)
+  make_element(x, lubridate::qday, ...)
 }
 
 on_week <- function(x, ...){
-  occuring_on(x, lubridate::week, ...)
+  make_element(x, lubridate::week, ...)
 }
 
 on_isoweek <- function(x, ...){
-  occuring_on(x, lubridate::isoweek, ...)
+  make_element(x, lubridate::isoweek, ...)
 }
 
 on_epiweek <- function(x, ...){
-  occuring_on(x, lubridate::epiweek, ...)
+  make_element(x, lubridate::epiweek, ...)
 }
 
