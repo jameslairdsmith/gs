2250b625aa91bc7f3bcdbeb2347e820febb06ab9
James Laird-Smith <jameslairdsmith@gmail.com>
2018-11-15 16:42:57 +0000

    added the schedule method for test_date().

--- R/test.R
+++ R/test.R
@@ -12,3 +12,21 @@ test_date.date_element <- function(date_element, date, ...){
 
   date_element$x == date_element$.f(date)
 }
+
+test_date.schedule <- function(schedule, date, ...){
+
+  if(!lubridate::is.instant(date)){
+    stop("'date' argument must be of type 'Date' or 'POSIX'", call. = F)
+  }
+    out <-
+      schedule %>%
+      purrr::modify_if(is_date_element, test_date, date) %>%
+      purrr::modify_if(is_schedule, test_date.schedule, date) %>%
+      purrr::modify_if(is_schedule, recon)
+
+    if(both_logical(out)){
+      recon(out)
+    } else {
+      test_date.schedule(out, date)
+    }
+}
