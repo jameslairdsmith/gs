46acca8db5050e7e0b135f0d2d6b66adcaeded48
James Laird-Smith <jameslairdsmith@gmail.com>
2019-02-16 19:28:06 +0000

    made changes to `on_day()` and `in_month()` functions to test against numbers, labels and abbreviated labels.

--- R/day.R
+++ R/day.R
@@ -1,7 +1,3 @@
-on_wday <- function(x, label = TRUE, abbr = TRUE, ...){
-  make_element(x, lubridate::wday, label = label, abbr = abbr, ...)
-}
-
 on_mday <- function(x, ...){
   make_element(x, lubridate::mday, ...)
 }
@@ -12,4 +8,18 @@ on_yday <- function(x, ...){
 
 on_qday <- function(x, ...){
   make_element(x, lubridate::qday, ...)
+}
+
+on_wday <- function(x, label = TRUE, abbr = TRUE, ...){
+  make_element(x, list(lubridate::wday,
+                       on_wday_label_abbr,
+                       on_wday_label_full), label = label, abbr = abbr, ...)
+}
+
+on_wday_label_abbr <- function(x){
+  lubridate::wday(x, label = T, abbr = T)
+}
+
+on_wday_label_full <- function(x){
+  lubridate::wday(x, label = T, abbr = F)
 }
--- R/month.R
+++ R/month.R
@@ -1,3 +1,18 @@
 in_month <- function(x, label = TRUE, abbr = TRUE, ...){
-  make_element(x, lubridate::month, label = label, abbr = abbr, ...)
+  make_element(x, list(lubridate::month,
+                       in_month_label_abbr,
+                       in_month_label_full), label = label, abbr = abbr, ...)
 }
+
+
+# month_plain <- function(x){
+#   lubridate::month(x)
+# }
+#
+ in_month_label_abbr <- function(x){
+   lubridate::month(x, label = TRUE, abbr = TRUE)
+ }
+#
+ in_month_label_full <- function(x){
+   lubridate::month(x, label = TRUE, abbr = FALSE)
+ }
