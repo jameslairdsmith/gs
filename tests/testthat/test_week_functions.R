b109a3d9a96166f055131aed55151060b08422ff
James Laird-Smith <jameslairdsmith@gmail.com>
2019-02-16 14:55:17 +0000

    corrected tests

--- tests/testthat/test_week_functions.R
+++ tests/testthat/test_week_functions.R
@@ -25,6 +25,13 @@ test_that("in_isoweek functions works", {
   expect_true(test_date(dmy("31/12/1990"), first_isoweek_of_year))
 })
 
-in_epiweek <- function(x, ...){
-  make_element(x, lubridate::epiweek, ...)
-}
+test_that("in_epiweek functions works", {
+  first_epiweek_of_year <- in_epiweek(1)
+  last_epiweek_of_year <- in_epiweek(52)
+
+  expect_true(test_date(dmy("01/01/2000"), last_isoweek_of_year))
+  expect_true(test_date(dmy("03/01/2000"), first_isoweek_of_year))
+  expect_false(test_date(dmy("02/01/2000"), first_isoweek_of_year))
+  expect_true(test_date(dmy("01/01/1990"), first_epiweek_of_year))
+  expect_true(test_date(dmy("31/12/1990"), first_epiweek_of_year))
+})
