6a2ff7cd466e59658fcffc0826a7b30694ac6499
James Laird-Smith <jameslairdsmith@gmail.com>
2019-02-16 13:56:57 +0000

    added tests

--- tests/testthat/test_day_functions.R
+++ tests/testthat/test_day_functions.R
@@ -7,4 +7,36 @@ test_that("on_yday function works", {
   first_day_of_year <- on_yday(1)
 
   expect_true(test_date(dmy("01/01/2000"), first_day_of_year))
+  expect_false(test_date(dmy("02/01/2000"), first_day_of_year))
+})
+
+test_that("on_qday function works", {
+
+  first_day_of_quarter <- on_qday(1)
+
+  expect_true(test_date(dmy("01/01/2000"), first_day_of_quarter))
+  expect_true(test_date(dmy("01/04/2000"), first_day_of_quarter))
+  expect_false(test_date(dmy("02/04/2000"), first_day_of_quarter))
+})
+
+test_that("on_mday function works", {
+
+  first_day_of_month <- on_mday(1)
+
+  expect_true(test_date(dmy("01/01/2000"), first_day_of_month))
+  expect_true(test_date(dmy("01/02/2000"), first_day_of_month))
+  expect_true(test_date(dmy("01/04/2000"), first_day_of_month))
+  expect_false(test_date(dmy("02/04/2000"), first_day_of_month))
+  expect_false(test_date(dmy("02/01/2000"), first_day_of_month))
+})
+
+test_that("on_wday function works", {
+
+  first_day_of_month <- on_wday("Thu")
+
+  expect_true(test_date(dmy("12/07/1990"), first_day_of_month))
+  expect_false(test_date(dmy("13/07/1990"), first_day_of_month))
+  expect_false(test_date(dmy("11/07/1990"), first_day_of_month))
+  expect_true(test_date(dmy("19/07/1990"), first_day_of_month))
+  expect_false(test_date(dmy("20/07/1990"), first_day_of_month))
 })
--- tests/testthat/test_occuring_functions.R
+++ tests/testthat/test_occuring_functions.R
@@ -8,3 +8,14 @@ test_that("only_occuring function works", {
   expect_false(test_date(dmy("11/07/1990"), is_my_birthday))
   expect_true(test_date(dmy("12/07/2019"), is_my_birthday))
 })
+
+test_that("also_occuring function works", {
+  is_my_birthday <- on_mday(12) %>% also_occuring(in_month("Jul"))
+
+  expect_true(test_date(dmy("12/07/1990"), is_my_birthday))
+  expect_true(test_date(dmy("13/07/1990"), is_my_birthday))
+  expect_true(test_date(dmy("11/07/1990"), is_my_birthday))
+  expect_true(test_date(dmy("12/07/2019"), is_my_birthday))
+  expect_true(test_date(dmy("12/06/2019"), is_my_birthday))
+  expect_false(test_date(dmy("11/06/2019"), is_my_birthday))
+})
