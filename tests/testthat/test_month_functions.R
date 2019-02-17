52757727e34ad1e53baff58343e3d186e41d4b8f
James Laird-Smith <jameslairdsmith@gmail.com>
2019-02-17 10:06:48 +0000

    added tests for `in_month` function accepting numbers or string numbers

--- tests/testthat/test_month_functions.R
+++ tests/testthat/test_month_functions.R
@@ -10,6 +10,26 @@ test_that("in_month function work", {
   expect_false(test_date(dmy("13/06/1990"), in_july))
 })
 
+test_that("in_month function works with numbers", {
+
+  in_july <- in_month(7)
+
+  expect_true(test_date(dmy("12/07/1990"), in_july))
+  expect_true(test_date(dmy("13/07/1990"), in_july))
+  expect_false(test_date(dmy("12/06/1990"), in_july))
+  expect_false(test_date(dmy("13/06/1990"), in_july))
+})
+
+test_that("in_month function works with string numbers", {
+
+  in_july <- in_month("7")
+
+  expect_true(test_date(dmy("12/07/1990"), in_july))
+  expect_true(test_date(dmy("13/07/1990"), in_july))
+  expect_false(test_date(dmy("12/06/1990"), in_july))
+  expect_false(test_date(dmy("13/06/1990"), in_july))
+})
+
 test_that("in_month function errors when given invalid spec", {
   expect_error(in_month("Febr"))
   expect_error(in_month(13))
