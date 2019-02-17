622ba345bb29519592da1a596972e63099fa57c6
James Laird-Smith <jameslairdsmith@gmail.com>
2019-02-17 09:55:23 +0000

    created new convenience function for locals

--- R/locals.R
+++ R/locals.R
@@ -10,6 +10,12 @@ get_month_abbr_names <- function(){
     magrittr::use_series(mon_ab)
 }
 
+get_all_month_specs <- function(){
+  c(1:12,
+    get_month_names(),
+    get_month_abbr_names())
+}
+
 get_day_names <- function(){
   readr::locale() %>%
     magrittr::use_series(date_names) %>%
@@ -21,3 +27,10 @@ get_day_abbr_names <- function(){
     magrittr::use_series(date_names) %>%
     magrittr::use_series(day_ab)
 }
+
+get_all_day_specs <- function(){
+  c(1:7,
+    get_day_names(),
+    get_day_abbr_names())
+}
+
