d809f655bc12ac8035d1ede1396f42c41dfdf20a
James Laird-Smith <jameslairdsmith@gmail.com>
2019-02-23 17:06:07 +0000

    changed `test_date()` function to allow it to compute on a vector of dates.

--- R/test.R
+++ R/test.R
@@ -6,10 +6,16 @@ test_date <- function(object, ...)
 
 test_date.Date <- function(date, x, ...){
 
-  test_date(x, date, ...)
+  temp_function <- purrr::partial(date_eval, x, ...)
+  #date_eval(x, date, ...)
+  purrr::map_lgl(date, temp_function, ...)
 }
 
-test_date.date_element <- function(date_element, date, ...){
+
+date_eval <- function(object, ...)
+  UseMethod("date_eval")
+
+date_eval.date_element <- function(date_element, date, ...){
 
   list_of_results <-
     purrr::map(date_element$.f, purrr::exec, date) %>%
@@ -21,9 +27,9 @@ test_date.date_element <- function(date_element, date, ...){
 
 }
 
-test_date.date_range_element <- function(date_range_element, date, ...){
+date_eval.date_range_element <- function(date_range_element, date, ...){
 
-  initial_test_result <- test_date.date_element(date_range_element$x, date)
+  initial_test_result <- date_eval.date_element(date_range_element$x, date)
 
   date_element_to_be_tested <- date_range_element$x
   n <- date_range_element$n
@@ -40,7 +46,7 @@ test_date.date_range_element <- function(date_range_element, date, ...){
       n_instances <- 1
       while(given_date_function(updated_date) == given_date_parameter){
         updated_date <- updated_date - days(1)
-          if(test_date(date_element_to_be_tested, updated_date)){
+          if(date_eval(date_element_to_be_tested, updated_date)){
             n_instances <- n_instances + 1
           }
       }
@@ -52,7 +58,7 @@ test_date.date_range_element <- function(date_range_element, date, ...){
     n_instances <- -1
     while(given_date_function(updated_date) == given_date_parameter){
       updated_date <- updated_date + days(1)
-      if(test_date(date_element_to_be_tested, updated_date)){
+      if(date_eval(date_element_to_be_tested, updated_date)){
         n_instances <- n_instances - 1
       }
     }
@@ -60,17 +66,17 @@ test_date.date_range_element <- function(date_range_element, date, ...){
   }
 }
 
-test_date.schedule <- function(schedule, date, ...){
+date_eval.schedule <- function(schedule, date, ...){
 
     out <-
       schedule %>%
-      purrr::modify_if(is_date_element, test_date, date) %>%
-      purrr::modify_if(is_schedule, test_date.schedule, date) %>%
+      purrr::modify_if(is_date_element, date_eval, date) %>%
+      purrr::modify_if(is_schedule, date_eval.schedule, date) %>%
       purrr::modify_if(is_schedule, recon)
 
     if(both_logical(out)){
       recon(out)
     } else {
-      test_date.schedule(out, date)
+      date_eval.schedule(out, date)
     }
 }
