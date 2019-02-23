f4cb35435f94f5435357d8a237dfbf35168cd64b
James Laird-Smith <jameslairdsmith@gmail.com>
2019-02-23 21:20:42 +0000

    added test for two combining schedules with `occuring` functions

--- tests/testthat/test_occuring_functions.R
+++ tests/testthat/test_occuring_functions.R
@@ -19,3 +19,17 @@ test_that("also_occuring function works", {
   expect_true(test_date(dmy("12/06/2019"), is_my_birthday))
   expect_false(test_date(dmy("11/06/2019"), is_my_birthday))
 })
+
+test_that("combined occuring functions work", {
+  my_birthday <- on_mday(12) %>% only_occuring(in_month("Jul"))
+  sisters_birthday <- on_mday(30) %>% only_occuring(in_month("Jul"))
+  our_birthdays <- my_birthday %>% also_occuring(sisters_birthday)
+
+  expect_true(test_date(dmy("12/07/1990"), our_birthdays))
+  expect_true(test_date(dmy("30/07/1992"), our_birthdays))
+  expect_false(test_date(dmy("11/07/1990"), our_birthdays))
+  expect_true(test_date(dmy("12/07/2019"), our_birthdays))
+  expect_true(test_date(dmy("30/07/2019"), our_birthdays))
+  expect_false(test_date(dmy("12/06/2019"), our_birthdays))
+  expect_false(test_date(dmy("11/06/2019"), our_birthdays))
+})
