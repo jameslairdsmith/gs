3cfa4c856f6c7315e48fc9f965c6438f4dc35f77
James Laird-Smith <jameslairdsmith@gmail.com>
2019-02-23 17:07:45 +0000

    corrected `project_dates()` function to account for the fact that `test_date()` function now takes a date as its first argument.

--- R/project.R
+++ R/project.R
@@ -2,5 +2,5 @@ project_dates <- function(x, from, to, ...){
 
   date_seq <- seq.Date(from = from, to = to, by = "1 day")
 
-  date_seq[test_date(x, date_seq)]
+  date_seq[test_date(date_seq, x)]
 }
