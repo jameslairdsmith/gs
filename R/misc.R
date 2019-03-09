de7c5cb9bc9922aa49b148fbfe92288cc0af5495
James Laird-Smith <jameslairdsmith@gmail.com>
2019-03-09 14:18:56 +0000

    moved `can_recon()` function

--- R/misc.R
+++ R/misc.R
@@ -67,17 +67,4 @@ or_schedule_early_recon <- function(or_schedule){
   } else {or_schedule}
 }
 
-can_recon <- function(x){
 
-  if(x %>% purrr::map_lgl(rlang::is_logical) %>% all()){
-    return(TRUE)
-  }
-
-  if(is_and_schedule(x)){
-     if(rlang::is_false(x[[1]]) | rlang::is_false(x[[2]])){return(TRUE)} else {return(FALSE)}
-  }
-
-  if(is_or_schedule(x)){
-     if(rlang::is_true(x[[1]]) | rlang::is_true(x[[2]])){return(TRUE)} else {return(FALSE)}
-  }
-}
--- R/recon.R
+++ R/recon.R
@@ -15,3 +15,18 @@ recon.and_schedule <- function(x){
 #
 #   !x[[1]]
 # }
+
+can_recon <- function(x){
+
+  if(x %>% purrr::map_lgl(rlang::is_logical) %>% all()){
+    return(TRUE)
+  }
+
+  if(is_and_schedule(x)){
+    if(rlang::is_false(x[[1]]) | rlang::is_false(x[[2]])){return(TRUE)} else {return(FALSE)}
+  }
+
+  if(is_or_schedule(x)){
+    if(rlang::is_true(x[[1]]) | rlang::is_true(x[[2]])){return(TRUE)} else {return(FALSE)}
+  }
+}
