e9516534a82edba8cc7250167c6668f4f8acff50
James Laird-Smith <jameslairdsmith@gmail.com>
2019-04-05 15:20:53 +0100

    edited recon methods so they co-operate with early return tests

--- R/recon.R
+++ R/recon.R
@@ -15,6 +15,7 @@ recon.and_schedule <- function(x){
   #x[[1]] & x[[2]]
 
   if(rlang::is_false(x[[1]]) | rlang::is_false(x[[2]])){return(FALSE)} else {return(TRUE)}
+  #if(x[[1]] == FALSE | x[[2]] == FALSE){return(FALSE)} else {return(TRUE)}
 }
 
 # recon.not_schedule <- function(x){
@@ -24,15 +25,19 @@ recon.and_schedule <- function(x){
 
 can_recon <- function(x){
 
-  if(x %>% purrr::map_lgl(rlang::is_logical) %>% all()){
-    return(TRUE)
-  }
+
 
   if(is_and_schedule(x)){
-    if(rlang::is_false(x[[1]]) | rlang::is_false(x[[2]])){return(TRUE)} else {return(FALSE)}
+    if(rlang::is_false(x[[1]]) | rlang::is_false(x[[2]])){return(TRUE)} #else {return(FALSE)}
   }
 
   if(is_or_schedule(x)){
-    if(rlang::is_true(x[[1]]) | rlang::is_true(x[[2]])){return(TRUE)} else {return(FALSE)}
+    if(rlang::is_true(x[[1]]) | rlang::is_true(x[[2]])){return(TRUE)} #else {return(FALSE)}
   }
+
+  if(x %>% purrr::map_lgl(rlang::is_logical) %>% all()){
+    return(TRUE)
+  }
+  FALSE
+
 }
