21d5a39c224916c95436b406c572fef0697daa96
James Laird-Smith <jameslairdsmith@gmail.com>
2019-04-07 13:42:32 +0100

    enhanced schedule() function so that it will accept years as well as dates.

--- R/schedule.R
+++ R/schedule.R
@@ -1,5 +1,13 @@
 schedule <- function(x, from, to, ...){
 
+  if(is.numeric(from)){
+    from <- lubridate::make_date(year = from)
+  }
+
+  if(is.numeric(to)){
+    to <- lubridate::make_date(year = to, month = 12, day = 31)
+  }
+
   date_seq <- seq.Date(from = from, to = to, by = "1 day")
 
   date_seq[test_date(date_seq, x)]
