623d8265ea9b4d6b23c4b02aebfce3c99df0c605
James Laird-Smith <jameslairdsmith@gmail.com>
2019-06-07 18:17:54 +0100

    resumed some tests

--- tests/testthat/test_date_functions.R
+++ tests/testthat/test_date_functions.R
@@ -10,22 +10,34 @@ test_that("on_date() function works", {
   expect_false(test_date(dmy("12/07/2019"), on_date(my_birthday)))
 })
 
-# test_that("after() function works", {
-#
-#   my_birthday <- on_mday(12) %>% only_occuring(in_month("Jul"))
-#
-#   expect_true(test_date(dmy("20/07/2019"), after(my_birthday, within_given = lubridate::month)))
-#   expect_false(test_date(dmy("07/07/2019"), after(my_birthday, within_given = lubridate::month)))
-# })
-#
-# test_that("after() function works with string for within_given", {
-#
-#   my_birthday <- on_mday(12) %>% only_occuring(in_month("Jul"))
-#
-#   expect_true(test_date(dmy("20/07/2019"), after(my_birthday, within_given = "month")))
-#   expect_false(test_date(dmy("07/07/2019"), after(my_birthday, within_given = "month")))
-# })
-#
+test_that("after() function works", {
+
+  my_birthday <- on_mday(12) %>% only_occuring(in_month("Jul"))
+
+  expect_true(test_date(dmy("20/07/2019"), after(my_birthday, within_given = lubridate::month)))
+  expect_false(test_date(dmy("07/07/2019"), after(my_birthday, within_given = lubridate::month)))
+})
+
+test_that("after() function works with string for within_given", {
+
+  my_birthday <- on_mday(12) %>% only_occuring(in_month("Jul"))
+
+  expect_true(test_date(dmy("20/07/2019"), after(my_birthday, within_given = "month")))
+  expect_false(test_date(dmy("07/07/2019"), after(my_birthday, within_given = "month")))
+})
+
+test_that("after() function works on a vector", {
+
+  my_birthday <- on_mday(12) %>% only_occuring(in_month("Jul"))
+
+  expected_result <- c(F, F, F, T, T)
+  days_around_birthday <- seq.Date(from = dmy("10/07/1990"), to = dmy("14/07/1990"), by = "1 day")
+
+  after_birthday <- after(my_birthday, within_given = "month")
+
+  expect_equal(test_date(days_around_birthday, after_birthday), expected_result)
+})
+
 # test_that("before() function works", {
 #
 #   my_birthday <- on_mday(12) %>% only_occuring(in_month("Jul"))
@@ -41,5 +53,5 @@ test_that("on_date() function works", {
 #   expect_false(test_date(dmy("20/07/2019"), before(my_birthday, within_given = "month")))
 #   expect_true(test_date(dmy("07/07/2019"), before(my_birthday, within_given = "month")))
 # })
-#
-#
+
+
--- tests/testthat/test_occuring_functions.R
+++ tests/testthat/test_occuring_functions.R
@@ -19,64 +19,64 @@ test_that("also_occuring function works", {
   expect_true(test_date(dmy("12/06/2019"), is_my_birthday))
   expect_false(test_date(dmy("11/06/2019"), is_my_birthday))
 })
-#
-# test_that("combined occuring functions work", {
-#   my_birthday <- on_mday(12) %>% only_occuring(in_month("Jul"))
-#   sisters_birthday <- on_mday(30) %>% only_occuring(in_month("Jul"))
-#   our_birthdays <- my_birthday %>% also_occuring(sisters_birthday)
-#
-#   expect_true(test_date(dmy("12/07/1990"), our_birthdays))
-#   expect_true(test_date(dmy("30/07/1992"), our_birthdays))
-#   expect_false(test_date(dmy("11/07/1990"), our_birthdays))
-#   expect_true(test_date(dmy("12/07/2019"), our_birthdays))
-#   expect_true(test_date(dmy("30/07/2019"), our_birthdays))
-#   expect_false(test_date(dmy("12/06/2019"), our_birthdays))
-#   expect_false(test_date(dmy("11/06/2019"), our_birthdays))
-# })
-#
-# test_that("not_occuring() function works on single element", {
-#   on_thursday <- on_wday("Thu")
-#   not_thursday <- not_occuring(on_thursday)
-#
-#   expect_true(test_date(dmy("12/07/1990"), on_thursday))
-#   expect_false(test_date(dmy("12/07/1990"), not_thursday))
-# })
-#
-# test_that("not_occuring() function works in nested structure", {
-#   weekdays_first_mil_week <-
-#     in_week(1) %>%
-#     not_occuring(on_wday("Sat")) %>%
-#     not_occuring(on_wday("Sun"))
-#
-#   expect_false(test_date(dmy("01/01/2000"), weekdays_first_mil_week))
-#   expect_true(test_date(dmy("03/01/2000"), weekdays_first_mil_week))
-#
-#   scheduled_dates <-
-#     schedule(weekdays_first_mil_week,
-#              from = dmy("25/12/1999"),
-#              to = dmy("15/01/2000"))
-#
-#   expect_equal(scheduled_dates, seq.Date(ymd("2000-01-03"),
-#                                          ymd("2000-01-07"),
-#                                          "1 day"))
-# })
-#
-# test_that("not_occuring() function works in started nested structure", {
-#   weekdays_first_mil_week <-
-#     not_occuring(on_wday("Sat")) %>%
-#     not_occuring(on_wday("Sun")) %>%
-#     only_occuring(in_week(1))
-#
-#
-#   expect_false(test_date(dmy("01/01/2000"), weekdays_first_mil_week))
-#   expect_true(test_date(dmy("03/01/2000"), weekdays_first_mil_week))
-#
-#   scheduled_dates <-
-#     schedule(weekdays_first_mil_week,
-#              from = dmy("25/12/1999"),
-#              to = dmy("15/01/2000"))
-#
-#   expect_equal(scheduled_dates, seq.Date(ymd("2000-01-03"),
-#                                          ymd("2000-01-07"),
-#                                          "1 day"))
-# })
+
+test_that("combined occuring functions work", {
+  my_birthday <- on_mday(12) %>% only_occuring(in_month("Jul"))
+  sisters_birthday <- on_mday(30) %>% only_occuring(in_month("Jul"))
+  our_birthdays <- my_birthday %>% also_occuring(sisters_birthday)
+
+  expect_true(test_date(dmy("12/07/1990"), our_birthdays))
+  expect_true(test_date(dmy("30/07/1992"), our_birthdays))
+  expect_false(test_date(dmy("11/07/1990"), our_birthdays))
+  expect_true(test_date(dmy("12/07/2019"), our_birthdays))
+  expect_true(test_date(dmy("30/07/2019"), our_birthdays))
+  expect_false(test_date(dmy("12/06/2019"), our_birthdays))
+  expect_false(test_date(dmy("11/06/2019"), our_birthdays))
+})
+
+test_that("not_occuring() function works on single element", {
+  on_thursday <- on_wday("Thu")
+  not_thursday <- not_occuring(on_thursday)
+
+  expect_true(test_date(dmy("12/07/1990"), on_thursday))
+  expect_false(test_date(dmy("12/07/1990"), not_thursday))
+})
+
+test_that("not_occuring() function works in nested structure", {
+  weekdays_first_mil_week <-
+    in_week(1) %>%
+    not_occuring(on_wday("Sat")) %>%
+    not_occuring(on_wday("Sun"))
+
+  expect_false(test_date(dmy("01/01/2000"), weekdays_first_mil_week))
+  expect_true(test_date(dmy("03/01/2000"), weekdays_first_mil_week))
+
+  scheduled_dates <-
+    schedule(weekdays_first_mil_week,
+             from = dmy("25/12/1999"),
+             to = dmy("15/01/2000"))
+
+  expect_equal(scheduled_dates, seq.Date(ymd("2000-01-03"),
+                                         ymd("2000-01-07"),
+                                         "1 day"))
+})
+
+test_that("not_occuring() function works in started nested structure", {
+  weekdays_first_mil_week <-
+    not_occuring(on_wday("Sat")) %>%
+    not_occuring(on_wday("Sun")) %>%
+    only_occuring(in_week(1))
+
+
+  expect_false(test_date(dmy("01/01/2000"), weekdays_first_mil_week))
+  expect_true(test_date(dmy("03/01/2000"), weekdays_first_mil_week))
+
+  scheduled_dates <-
+    schedule(weekdays_first_mil_week,
+             from = dmy("25/12/1999"),
+             to = dmy("15/01/2000"))
+
+  expect_equal(scheduled_dates, seq.Date(ymd("2000-01-03"),
+                                         ymd("2000-01-07"),
+                                         "1 day"))
+})
