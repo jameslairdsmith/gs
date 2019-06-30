0645b72ebf79f28350f0f108e8609895ebb18933
James Laird-Smith <jameslairdsmith@gmail.com>
2019-06-30 15:58:11 +0100

    added test for in_month() function taking mixed month specs.

--- tests/testthat/test_month_functions.R
+++ tests/testthat/test_month_functions.R
@@ -97,3 +97,17 @@ test_that("in_month function works with mutliple months specified as integers", 
 
   expect_equal(schedule(Jan_Feb_and_Dec, during = 2000), expected_dates)
 })
+
+test_that("in_month function works with mixed month specifications", {
+
+  expected_dates <- c(seq.Date(from = dmy("01/01/2000"),
+                               to = dmy("29/02/2000"),
+                               by = "1 day"),
+                      seq.Date(from = dmy("01/12/2000"),
+                               to = dmy("31/12/2000"),
+                               by = "1 day"))
+
+  Jan_Feb_and_Dec <- in_month(1, "Feb", "December")
+
+  expect_equal(schedule(Jan_Feb_and_Dec, during = 2000), expected_dates)
+})
