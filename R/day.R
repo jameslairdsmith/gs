1c5cca1809e4145139298baabc6670a3a23b6529
James Laird-Smith <jameslairdsmith@gmail.com>
2019-06-30 15:33:03 +0100

    clean up code

--- R/day.R
+++ R/day.R
@@ -12,13 +12,10 @@ on_qday <- function(x, ...){
 
 on_wday <- function(x, override_name_check = FALSE, ...){
 
-  #if(override_name_check == FALSE){
-
     if(!(x %in% get_all_day_specs())){
       stop("x is not a legitimate day name")
     }
 
-  #}
 
   if(x %in% 1:7){
     appro_function <- lubridate::wday}
@@ -28,10 +25,6 @@ on_wday <- function(x, override_name_check = FALSE, ...){
 
   if(x %in% get_day_abbr_names()){
     appro_function <- on_wday_label_abbr}
-
-  # make_element(x, list(lubridate::wday,
-  #                      on_wday_label_abbr,
-  #                      on_wday_label_full), ...)
 
   make_element(x, appro_function, ...)
 }
