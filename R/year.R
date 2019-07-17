16527e216ee84a64783e8fc3533ff170199b30c0
James Laird-Smith <jameslairdsmith@gmail.com>
2019-07-17 17:47:17 +0100

    clean up code

--- R/year.R
+++ R/year.R
@@ -1,11 +1,26 @@
-in_year <- function(x, ...){
-  make_element(x, lubridate::year, ...)
+in_year <- function(...){
+
+  x <- unlist(list(...))
+
+  if(length(x) > 1) return(check_vec_loop(x, in_year))
+
+  make_element(x, lubridate::year)
 }
 
-in_isoyear <- function(x, ...){
-  make_element(x, lubridate::isoyear, ...)
+in_isoyear <- function(...){
+
+  x <- unlist(list(...))
+
+  if(length(x) > 1) return(check_vec_loop(x, in_isoyear))
+
+  make_element(x, lubridate::isoyear)
 }
 
-in_epiyear <- function(x, ...){
-  make_element(x, lubridate::epiyear, ...)
+in_epiyear <- function(...){
+
+  x <- unlist(list(...))
+
+  if(length(x) > 1) return(check_vec_loop(x, in_epiyear))
+
+  make_element(x, lubridate::epiyear)
 }
