d04630d035fd4e88c437510598597e38015c75bb
James Laird-Smith <jameslairdsmith@gmail.com>
2019-07-19 15:01:25 +0100

    added documentation for the `on_day` functions.

--- R/day.R
+++ R/day.R
@@ -1,3 +1,20 @@
+#' Specify the days of a schedule
+#'
+#' Creates a schedule of events occuring on the types of days specified.
+#'
+#' `on_weekend()` is a convenience function for
+#' `on_wday("Sat", "Sun")`
+#'
+#' @param ... a numeric vector of day elements. In the case of `on_wday` the
+#' elements can also be characters (see details below).
+#' @param week_start
+#' You can use `lubridate`'s global option `lubridate.week.start` to set this
+#' parameter globally
+#'
+#' @keywords month, date, scedule
+#' @return A schedule of events occuring on the day types specified.
+#' @export
+
 on_mday <- function(...){
 
   x <- unlist(list(...))
@@ -6,6 +23,9 @@ on_mday <- function(...){
 
   make_element(x, lubridate::mday)
 }
+
+#' @rdname on_mday
+#' @export
 
 on_yday <- function(...){
 
@@ -24,6 +44,9 @@ on_qday <- function(...){
 
   make_element(x, lubridate::qday)
 }
+
+#' @rdname on_mday
+#' @export
 
 on_wday <- function(..., week_start = getOption("lubridate.week.start", 7)){
 
@@ -47,6 +70,8 @@ on_wday <- function(..., week_start = getOption("lubridate.week.start", 7)){
   make_element(x, appro_function)
 
 }
+#' @rdname on_mday
+#' @export
 
 on_weekend <- function(){
   on_wday("Sat", "Sun")
