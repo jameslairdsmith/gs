39f0eacfc77dc1503a0b3a6d4b8c60c66995c4bc
James Laird-Smith <jameslairdsmith@gmail.com>
2019-07-19 17:28:11 +0100

    documented day functions.

--- R/day.R
+++ R/day.R
@@ -34,25 +34,51 @@
 #'         - `on_wday("Sat")` for Saturdays.
 #'         - `on_wday("Wed")` for Wednesdays.
 #'
-#' Users can specify multiple day elements in a single call. For example
-#' `on_mday(1, 3)` produces a schedule of dates occuring on the 1st and 3rd
-#' days of every month. Likewise `on_wday("Tue", "Thu")` produces a schedule
-#' of events occuring every week on both Tuesdays and Thursdays.
+#' All functions accept multiple day elements in a single call. For example
+#' `on_mday(7, 9)` produces a schedule of dates occuring on the 7th and 9th
+#' days of every month. Likewise `on_mday(1:5)` produces a schedule of dates
+#' occuring on the first five days of every month. Similarly
+#'  `on_wday("Tue", "Thu")` produces a schedule of events occuring every
+#'  week on both Tuesdays and Thursdays.
 #'
 #' `on_weekend()` is a convenience function for
 #' `on_wday("Sat", "Sun")`
 #'
-#' @param ... A numeric vector of day elements. In the case of `on_wday` the
+#' @param ... A numeric vector of day specifications. In the case of `on_wday` the
 #' elements can also be characters (see details below).
 #' @param week_start If using the `on_wday` function with numeric day elements,
 #' you can specify which ISO convention is used; 1 means Monday,
-#' 7 means Sunday (default)
+#' 7 means Sunday (default).
+#' @param date_vec Optional. A vector of dates to test for whether they are on
+#' the weekend. If missing, will simply return a schedule of weekend days.
 #'
 #' You can use `lubridate`'s global option
 #' `lubridate.week.start` to set this parameter globally.
 #'
 #' @keywords month, date, scedule
 #' @return A schedule of events occuring on the day types specified.
+#' @examples
+#'
+#' my_dates <- seq.Date(from = as.Date("2000-01-01"),
+#'                      to = as.Date("2000-12-01"),
+#'                      by = "1 month")
+#' my_dates
+#'
+#' on_first_day_of_month <- on_mday(1)
+#' on_first_day_of_month(my_dates)
+#'
+#' on_first_day_of_year <- on_yday(1)
+#' on_first_day_of_year(my_dates)
+#'
+#' on_first_day_of_quarter <- on_qday(1)
+#' on_first_day_of_quarter(my_dates)
+#'
+#' on_tuesday <- on_wday("Tue")
+#' on_tuesday(my_dates)
+#'
+#' on_weekend(my_dates)
+#'
+#'
 #' @export
 
 on_mday <- function(...){
@@ -116,8 +142,12 @@ on_wday <- function(..., week_start = getOption("lubridate.week.start", 7)){
 #' @rdname on_mday
 #' @export
 
-on_weekend <- function(){
-  on_wday("Sat", "Sun")
+on_weekend <- function(date_vec = NULL){
+  out_func <- on_wday("Sat", "Sun")
+
+  if(!missing(date_vec)) return(out_func(date_vec))
+
+  out_func
 }
 
 on_wday_label_abbr <- function(x, ...){
