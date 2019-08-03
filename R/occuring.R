a213f02cc4a06184eea3dd9fc4542ef9002df81d
James Laird-Smith <jameslairdsmith@gmail.com>
2019-08-03 14:18:27 +0100

    also_occuring functions now calculates appropriate start and end dates

--- R/occuring.R
+++ R/occuring.R
@@ -17,7 +17,7 @@ also_occuring <- function(elem_1, elem_2){
 
 only_occuring <- function(elem_1, elem_2){
 
-  envoke_list <- list(elem_2, elem_1)
+  #envoke_list <- list(elem_2, elem_1)
 
   out <- function(date){
     elem_1(date) & elem_2(date)
@@ -37,6 +37,17 @@ only_occuring <- function(elem_1, elem_2){
   if("earliest_date" %in% get_attribute_names(elem_2)){
     attr(out, "earliest_date") <- attr(elem_2, "earliest_date")
   }
+  if("latest_date" %in% get_attribute_names(elem_1) &
+     "latest_date" %in% get_attribute_names(elem_2)){
+    attr(out, "latest_date") <- max(attr(elem_1, "latest_date"),
+                                    attr(elem_2, "latest_date"))
+  }
+  if("earliest_date" %in% get_attribute_names(elem_1) &
+     "earliest_date" %in% get_attribute_names(elem_2)){
+    attr(out, "earliest_date") <- min(attr(elem_1, "earliest_date"),
+                                      attr(elem_2, "earliest_date"))
+  }
+
 
   out
 }
