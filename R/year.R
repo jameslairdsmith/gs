41877403b5884c1c5e4bef5c2b9fab83237a8b95
James Laird-Smith <jameslairdsmith@gmail.com>
2019-08-03 14:18:54 +0100

    in_year() function now encodes earlist and latest dates.

--- R/year.R
+++ R/year.R
@@ -1,11 +1,37 @@
+
+
+
 in_year <- function(...){
 
   x <- unlist(list(...))
 
-  if(length(x) > 1) return(check_vec_loop(x, in_year))
+  if(length(x) > 1){
 
-  make_element(x, lubridate::year)
+    final_output <- check_vec_loop(x, in_year)
+
+    earliest_date <- lubridate::make_date(year = min(x), month = 1, day = 1)
+    latest_date <- lubridate::make_date(year = max(x), month = 12, day = 31)
+
+    attr(final_output, "earliest_date") <- earliest_date
+    attr(final_output, "latest_date") <- latest_date
+
+    return(final_output)
+    }
+
+  output <- make_element(x, lubridate::year)
+
+  earliest_date <- lubridate::make_date(year = min(x), month = 1, day = 1)
+  latest_date <- lubridate::make_date(year = max(x), month = 12, day = 31)
+
+  attr(output, "earliest_date") <- earliest_date
+  attr(output, "latest_date") <- latest_date
+
+  output
 }
+
+
+
+
 
 in_isoyear <- function(...){
 
