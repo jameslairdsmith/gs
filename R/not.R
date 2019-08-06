9e91ba19157e3c5a03cd82ce99a26b6207e2bbc7
James Laird-Smith <jameslairdsmith@gmail.com>
2019-08-06 19:13:20 +0100

    put not_occuring() documentation into its own file.

---
+++ R/not.R
@@ -0,0 +1,64 @@
+#' Invert a schedule
+#'
+#' @description
+#'
+#' These functions combine schedules by taking two of them as inputs and
+#' returning a single combined schedule as output.
+#'
+#' @param x,y Schedules to weave together.
+#'
+#' @details
+#' `not_occuring()` can accept either one or two schedules as input(s):
+#' * When a single schedule is used, the function returns a schedule of all
+#' events that do not fall on on the input schedule. The result is the
+#' **negation** of the schedule. For example, the negation of all weekdays
+#' would be a schedule of all weekend days.
+#' * When two schedules are used, the function returns the first schedule but
+#'  with the events of the second schedule stripped away. This works to remove
+#'  the events of the second schedule from the first.
+#'    - This means that `not_occuring(x, y)` is the equivalent of
+#'      `only_occuring(x, not_occuring(y))`
+#'
+#' @return A schedule of events determined by the input schedules and rules
+#' of the function used.
+#' @examples
+#' on_christmas <- only_occuring(on_mday(25), in_month("Dec"))
+#' on_new_years_day <- on_yday(1)
+#'
+#' on_public_holidays <- also_occuring(on_new_years_day, on_christmas)
+#'
+#' schedule(on_public_holidays, from = 2000, to = 2004)
+#'
+#' weekday_public_holidays <- only_occuring(on_public_holidays,
+#'                                          not_occuring(on_wday("Sat", "Sun")))
+#'
+#' schedule(weekday_public_holidays, from = 2000, to = 2004)
+#'
+#' @export
+
+not_occuring <- function(x, y = NULL){
+
+  elem_1 <- x
+  elem_2 <- y
+
+  if(is.null(elem_2)){
+
+    out <- function(date){
+      !elem_1(date)
+    }
+
+    class(out) <- c("schedule")
+
+    out
+
+  } else {
+
+    out <- function(date){
+      !elem_2(date)
+    }
+
+    class(out) <- c("schedule")
+
+    elem_1 %>% only_occuring(out)
+  }
+}
--- R/occuring.R
+++ R/occuring.R
@@ -16,17 +16,6 @@
 #' present in both the first schedule (`x`) and in the second schedule
 #' (`y`). The result is the **intersection** of the two schedules.
 #'
-#' `not_occuring()` can accept either one or who schedules as input(s):
-#' * When a single schedule is used, the function returns a schedule of all
-#' events that do not fall on on the input schedule. The result is the
-#' **negation** of the schedule. For example, the negation of all weekdays
-#' would be a schedule of all weekend days.
-#' * When two schedules are used, the function returns the first schedule but
-#'  with the events of the second schedule stripped away. This works to remove
-#'  the events of the second schedule from the first.
-#'    - This means that `not_occuring(x, y)` is the equivalent of
-#'      `only_occuring(x, not_occuring(y))`
-#'
 #' @return A schedule of events determined by the input schedules and rules
 #' of the function used.
 #' @examples
@@ -39,12 +28,6 @@
 #' on_public_holidays <- also_occuring(on_new_years_day, on_christmas)
 #'
 #' schedule(on_public_holidays, from = 2000, to = 2004)
-#'
-#' weekday_public_holidays <- only_occuring(on_public_holidays,
-#'                                          not_occuring(on_wday("Sat", "Sun")))
-#'
-#' schedule(weekday_public_holidays, from = 2000, to = 2004)
-#'
 #' @export
 also_occuring <- function(x, y){
 
@@ -106,32 +89,3 @@ only_occuring <- function(x, y){
   out
 }
 
-#' @rdname also_occuring
-#' @export
-
-not_occuring <- function(x, y = NULL){
-
-  elem_1 <- x
-  elem_2 <- y
-
-  if(is.null(elem_2)){
-
-    out <- function(date){
-      !elem_1(date)
-    }
-
-    class(out) <- c("schedule")
-
-    out
-
-  } else {
-
-    out <- function(date){
-      !elem_2(date)
-    }
-
-    class(out) <- c("schedule")
-
-    elem_1 %>% only_occuring(out)
-  }
-}
