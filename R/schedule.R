bb4a930ef6cde867547874d5983f1a42fa1f49a4
James Laird-Smith <jameslairdsmith@gmail.com>
2019-08-11 14:34:22 +0100

    added new schedule_* functions

--- R/schedule.R
+++ R/schedule.R
@@ -13,30 +13,12 @@
 #' schedule(on_first_day_month, during = 2000)
 #' @export
 
-schedule <- function(x, from = NULL, to = NULL, during = NULL, ...){
+schedule <- function(x, from = NULL, to = NULL, during = NULL, every = "1 day",...){
 
-  if("earliest_date" %in% get_attribute_names(x)){
-    from <- attr(x, "earliest_date")
-  }
+  from <- get_from(x = x, from = from, during = during)
+  to <- get_to(x = x, to = to, during = during)
 
-  if("latest_date" %in% get_attribute_names(x)){
-    to <- attr(x, "latest_date")
-  }
-
-  if(!is.null(during)){
-    from <- lubridate::make_date(year = during)
-    to <- lubridate::make_date(year = during, month = 12, day = 31)
-  }
-
-  if(is.numeric(from)){
-    from <- lubridate::make_date(year = from)
-  }
-
-  if(is.numeric(to)){
-    to <- lubridate::make_date(year = to, month = 12, day = 31)
-  }
-
-  date_seq <- seq.Date(from = from, to = to, by = "1 day")
+  date_seq <- seq.Date(from = from, to = to, by = every)
 
   date_seq[test_date(date_seq, x)]
 }
@@ -44,20 +26,18 @@ schedule <- function(x, from = NULL, to = NULL, during = NULL, ...){
 #' @rdname schedule
 #' @export
 
-schedule_hours <- function(x, from, to, during = NULL, ...){
+schedule_days <- function(x, from = NULL, to = NULL, during = NULL, ...){
 
-  if(!is.null(during)){
-    from <- lubridate::make_date(year = during)
-    to <- lubridate::make_date(year = during, month = 12, day = 31)
-  }
+  schedule(x = x, from = from, to = to, during = during, every = "1 day", ...)
+}
 
-  if(is.numeric(from)){
-    from <- lubridate::make_date(year = from)
-  }
+#' @rdname schedule
+#' @export
 
-  if(is.numeric(to)){
-    to <- lubridate::make_date(year = to, month = 12, day = 31)
-  }
+schedule_hours <- function(x, from = NULL, to = NULL, during = NULL, ...){
+
+  from <- get_from(x = x, from = from, during = during)
+  to <- get_to(x = x, to = to, during = during)
 
   to <- as_datetime(to)
   from <- as_datetime(from)
@@ -65,4 +45,46 @@ schedule_hours <- function(x, from, to, during = NULL, ...){
   datetime_seq <- seq.POSIXt(from = from, to = to, by = "1 hour")
 
   datetime_seq[test_date(datetime_seq, x)]
+}
+
+get_from <- function(x, from, during){
+
+  if("earliest_date" %in% get_attribute_names(x)){
+    return_from <- attr(x, "earliest_date")
+  }
+
+  if(!is.null(during)){
+    return_from <- lubridate::make_date(year = during)
+  }
+
+  if(is.numeric(from)){
+    return_from <- lubridate::make_date(year = from)
+  }
+
+  if(lubridate::is.Date(from)){
+    return_from <- from
+  }
+
+  if(exists("return_from")){return(return_from)}else{return(NULL)}
+}
+
+get_to <- function(x, to, during){
+
+  if("latest_date" %in% get_attribute_names(x)){
+    return_to <- attr(x, "latest_date")
+  }
+
+  if(!is.null(during)){
+    return_to <- lubridate::make_date(year = during, month = 12, day = 31)
+  }
+
+  if(is.numeric(to)){
+    return_to <- lubridate::make_date(year = to, month = 12, day = 31)
+  }
+
+  if(lubridate::is.Date(to)){
+    return_to <- to
+  }
+
+  if(exists("return_to")){return(return_to)}else{return(NULL)}
 }
--- tests/testthat/test_schedule_function.R
+++ tests/testthat/test_schedule_function.R
@@ -25,13 +25,29 @@ test_that("schedule function works with year abbreviations", {
 
 test_that("schedule function works when specifying only before() and after()", {
 
-  demo_dates <- seq.Date(from = dmy("02/07/1990"), to = dmy("30/07/1990"), by = "1 day")
+  demo_dates <- seq.Date(from = dmy("02/07/1990"),
+                         to = dmy("30/07/1990"),
+                         by = "1 day")
 
-  before_and_after <- after(dmy("01/07/1990")) %>% only_occurring(before(dmy("31/07/1990")))
+  before_and_after <- after(dmy("01/07/1990")) %>%
+    only_occurring(before(dmy("31/07/1990")))
 
   expect_true("earliest_date" %in% get_attribute_names(before_and_after))
   expect_true("latest_date" %in% get_attribute_names(before_and_after))
 
   expect_equal(schedule(before_and_after), demo_dates)
 
+})
+
+test_that("schedule_hours() function works", {
+
+  christmas <- on_mday(25) %>% only_occurring(in_month("Dec"))
+
+  one <- christmas %>% schedule_hours(during = 2000)
+
+  two <- seq.Date(from = dmy("25/12/2000"),
+                  to = dmy("25/12/2000"),
+                  by = "1 hour")
+
+  expect_identical(one, two)
 })
