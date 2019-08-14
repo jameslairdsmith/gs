c41b46eeb28e70ad3ed200975de99d8b2b86fc2b
James Laird-Smith <jameslairdsmith@gmail.com>
2019-08-14 19:27:31 +0100

    added "day" and "week" options for period strings.

--- R/misc.R
+++ R/misc.R
@@ -1,6 +1,8 @@
 strings_to_date_functions <- function(str){
   str <- str
   .f <- dplyr::case_when(
+    str == "day" ~ quote(lubridate::day),
+    str == "week" ~ quote(lubridate::week),
     str == "month" ~ quote(lubridate::month),
     str == "quarter" ~ quote(lubridate::quarter),
     str == "year" ~ quote(lubridate::year),
