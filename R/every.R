b319312540119ad5ebfb4965172d0a80f79a156e
James Laird-Smith <jameslairdsmith@gmail.com>
2019-09-01 11:47:45 +0100

    made progress with on_every_n() function.

--- R/every.R
+++ R/every.R
@@ -1,25 +1,49 @@
-every_n <- function(n, unit, since){
+on_every_n <- function(n, unit, starting, inclusive = T, backdated = F){
 
-  unit_freq <- unit(n)
+  if(class(unit) == "function"){
 
-  if(class(unit_freq) == "Period"){
+    unit_freq <- unit(n)
 
-    date_test <- function(date){
+      date_test <- function(date){
 
-      between_dates <- as.period(date - since)
-      is_whole_number(between_dates / unit_freq)
-    }
+        my_interval <- interval(starting, date)
 
-    out <- list(date_test = date_test)
+        period_period <- as.period(my_interval) / unit_freq
 
-    class(out) <- "schedule"
+        interval_period <- my_interval / unit_freq
 
-    out$terms <- 1
+        result <- is_whole_number(period_period) | is_whole_number(interval_period)
 
-    out
+        if(inclusive == F){
+          result[period_period == 0] <- FALSE
+          result[interval_period == 0] <- FALSE
+        }
+
+        if(backdated == F){
+          result[period_period < 0] <- FALSE
+          result[interval_period < 0] <- FALSE
+        }
+
+        result
+
+      }
+
+      out <- list(date_test = date_test)
+
+      class(out) <- "schedule"
+
+      out$terms <- 1
+
+      out
   }
+
+
 }
 
-every <- function(unit, since){
-  every_n(1, unit = unit, since = since)
+on_every <- function(unit, starting, inclusive = T, backdated = F){
+  every_n(1,
+          unit = unit,
+          starting = starting,
+          inclusive = inclusive,
+          backdated = backdated)
 }
