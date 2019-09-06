c74efcd35d5c04800305b2d1eb29ea1a9d8099a4
James Laird-Smith <jameslairdsmith@gmail.com>
2019-09-06 17:49:31 +0100

    completed work on_every_nth() functions.

--- R/every.R
+++ R/every.R
@@ -4,27 +4,45 @@
 #' Create a schedule of events occuring in increments from a fixed date.
 #'
 #' @param n A single integer specifying the increment of the events to create.
-#' @param unit The type of increment. Can be either:
-#'    * A `lubridate` period object eg. "lubridate::months".
-#'    * A schedule object.
+#' Eg. `2L` for every second event.
+#' @param unit The type of period to increment. Can be either:
+#'    * A character shortcut for a period object. Eg. "year", "years", "month",
+#'    "months", "week", "weeks" etc. Can be any value accepted by
+#'    `lubridate::period()`.
+#'    * A schedule object. Eg. `on_weekday()` for events occuring every `n`
+#'    weekdays.
 #' @param starting The first event of the series. Can be a date or datetime.
 #' @param inclusive Logical indicating whether the `starting` date should
 #' be included in the resulting schedule.
 #' @param backdated Logical indicating whether dates prior the `starting`
 #' date should be included in the resulting schedule.
+#' @details
+#' `on_every()` and `on_every_second()` are convenience functions for
+#' `on_every_nth()` where the `n` argument is pre-filled with `1L` and `2L`
+#' respectively.
 #' @return A schedule object.
+#' @examples
+#' monthly_from_millenium_start <- on_every("month", as.Date("2000-01-01"))
+#' schedule_days(monthly_from_millenium_start, during = 2000)
+#'
+#' fortnightly_from_millenium_start <- on_every_second("week", as.Date("2000-01-01"))
+#' schedule_days(fortnightly_from_millenium_start, during = 2000)
+#'
+#' tenth_weekend_day_from_millenium_start <- on_every_nth(10, on_weekend(), as.Date("2000-01-01"))
+#' schedule_days(tenth_weekend_day_from_millenium_start, during = 2000)
+#' @export
 
 on_every_nth <- function(n, unit, starting, inclusive = TRUE, backdated = FALSE){
 
-  if(class(unit) == "function"){
+  if(class(unit) == "character"){
 
-    unit_freq <- unit(n)
+    unit_freq <- lubridate::period(n, units = unit)
 
       date_test <- function(date){
 
-        my_interval <- interval(starting, date)
+        my_interval <- lubridate::interval(starting, date)
 
-        period_period <- suppressWarnings(as.period(my_interval) / unit_freq)
+        period_period <- suppressMessages(lubridate::as.period(my_interval) / unit_freq)
 
         interval_period <- my_interval / unit_freq
 
@@ -55,7 +73,7 @@ on_every_nth <- function(n, unit, starting, inclusive = TRUE, backdated = FALSE)
 
   if(class(unit) == "schedule"){
 
-    if(!happen(unit, starting)){stop("starting date isn't on schedule")}
+    if(!happen(unit, starting)){stop("starting date does not fall on schedule")}
 
     date_test <- function(date){
 
@@ -101,7 +119,18 @@ on_every_nth <- function(n, unit, starting, inclusive = TRUE, backdated = FALSE)
 #' @export
 
 on_every <- function(unit, starting, inclusive = TRUE, backdated = FALSE){
-  on_every_nth(1,
+  on_every_nth(1L,
+               unit = unit,
+               starting = starting,
+               inclusive = inclusive,
+               backdated = backdated)
+}
+
+#' @rdname on_every_nth
+#' @export
+
+on_every_second <- function(unit, starting, inclusive = TRUE, backdated = FALSE){
+  on_every_nth(2L,
                unit = unit,
                starting = starting,
                inclusive = inclusive,
--- tests/testthat/test_every_functions.R
+++ tests/testthat/test_every_functions.R
@@ -4,7 +4,7 @@ library(lubridate)
 test_that("on_every() function works with months", {
 
   produced_result <-
-    on_every(months, dmy("28/02/2001"), inclusive = T, backdated = T) %>%
+    on_every("months", dmy("28/02/2001"), inclusive = T, backdated = T) %>%
     schedule_days(during = 2001)
 
   expected_result <- dmy("28/02/2001") %m+% months(-1:10)
@@ -13,10 +13,32 @@ test_that("on_every() function works with months", {
 
 })
 
+test_that("on_every() function works with month singular", {
+
+  produced_result <-
+    on_every("month", dmy("28/02/2001"), inclusive = T, backdated = T) %>%
+    schedule_days(during = 2001)
+
+  expected_result <- dmy("28/02/2001") %m+% months(-1:10)
+
+  expect_equal(produced_result, expected_result)
+
+})
+
+test_that("on_every_n() function errors with month fraction", {
+
+    expect_error(on_every_nth(1.5,
+                              "month",
+                              dmy("28/02/2001"),
+                              inclusive = T,
+                              backdated = T))
+
+})
+
 test_that("on_every() function works with months in leap year", {
 
   produced_result <-
-    on_every(months, dmy("29/02/2000"), inclusive = T, backdated = T) %>%
+    on_every("months", dmy("29/02/2000"), inclusive = T, backdated = T) %>%
     schedule_days(during = 2000)
 
   expected_result <- dmy("29/02/2000") %m+% months(-1:10)
@@ -28,7 +50,7 @@ test_that("on_every() function works with months in leap year", {
 test_that("on_every() function works with months of 30th", {
 
   produced_result <-
-    on_every(months, dmy("30/01/2000"), inclusive = T, backdated = T) %>%
+    on_every("months", dmy("30/01/2000"), inclusive = T, backdated = T) %>%
     schedule_days(during = 2000)
 
   expected_result <- dmy("30/01/2000") %m+% months(0:11)
@@ -40,7 +62,7 @@ test_that("on_every() function works with months of 30th", {
 test_that("on_every() function works with last days of month", {
 
   produced_result <-
-    on_every(months, dmy("31/01/2000"), inclusive = T, backdated = T) %>%
+    on_every("months", dmy("31/01/2000"), inclusive = T, backdated = T) %>%
     schedule_days(during = 2000)
 
   expected_result <- dmy("31/01/2000") %m+% months(0:11)
@@ -52,7 +74,7 @@ test_that("on_every() function works with last days of month", {
 test_that("on_every() function works with years", {
 
   produced_result <-
-    on_every(years, dmy("28/02/2001"), inclusive = T, backdated = T) %>%
+    on_every("years", dmy("28/02/2001"), inclusive = T, backdated = T) %>%
     schedule_days(from = 2001, to = 2007)
 
   expected_result <- dmy("28/02/2001") %m+% years(0:6)
@@ -64,7 +86,7 @@ test_that("on_every() function works with years", {
 test_that("on_every() function works with leap years", {
 
   produced_result <-
-    on_every(years, dmy("29/02/2000"), inclusive = T, backdated = T) %>%
+    on_every("years", dmy("29/02/2000"), inclusive = T, backdated = T) %>%
     schedule_days(from = 2000, to = 2006)
 
   expected_result <- dmy("29/02/2000") %m+% years(0:6)
@@ -76,7 +98,7 @@ test_that("on_every() function works with leap years", {
 test_that("on_every() function works with days", {
 
   produced_result <-
-    on_every(days, dmy("28/02/2001"), inclusive = T, backdated = F) %>%
+    on_every("days", dmy("28/02/2001"), inclusive = T, backdated = F) %>%
     schedule_days(from = 2001, to = 2001)
 
   expected_result <- seq.Date(from = dmy("28/02/2001"),
@@ -91,7 +113,7 @@ test_that("on_every() function works with days", {
 test_that("on_every() function works with weeks", {
 
   produced_result <-
-    on_every(weeks, dmy("28/02/2001"), inclusive = T, backdated = F) %>%
+    on_every("weeks", dmy("28/02/2001"), inclusive = T, backdated = F) %>%
     schedule_days(from = 2001, to = 2001)
 
   expected_result <- seq.Date(from = dmy("28/02/2001"),
@@ -105,7 +127,7 @@ test_that("on_every() function works with weeks", {
 test_that("on_every() function works with weeks in leap year", {
 
   produced_result <-
-    on_every(weeks, dmy("29/02/2000"), inclusive = T, backdated = F) %>%
+    on_every("weeks", dmy("29/02/2000"), inclusive = T, backdated = F) %>%
     schedule_days(from = 2000, to = 2000)
 
   expected_result <- seq.Date(from = dmy("29/02/2000"),
@@ -114,4 +136,31 @@ test_that("on_every() function works with weeks in leap year", {
 
   expect_equal(produced_result, expected_result)
 
+})
+
+test_that("on_every_nth() function works with a schedule", {
+
+  produced_result <-
+    on_every_nth(10, on_weekend(), dmy("01/01/2000"), inclusive = T, backdated = F) %>%
+    schedule_days(from = 2000, to = 2000)
+
+  expected_result <- c(as.Date("2000-01-01"),
+                       as.Date("2000-02-05"),
+                       as.Date("2000-03-11"),
+                       as.Date("2000-04-15"),
+                       as.Date("2000-05-20"),
+                       as.Date("2000-06-24"),
+                       as.Date("2000-07-29"),
+                       as.Date("2000-09-02"),
+                       as.Date("2000-10-07"),
+                       as.Date("2000-11-11"),
+                       as.Date("2000-12-16"))
+
+  expect_equal(produced_result, expected_result)
+
+})
+
+test_that("on_every_nth() function errors with a start date not on schedule", {
+
+    expect_error(on_every_nth(10, on_weekday(), dmy("01/01/2000")))
 })
