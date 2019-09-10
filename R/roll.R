70520f7aa18869ce628108b31c43dabfeb330525
James Laird-Smith <jameslairdsmith@gmail.com>
2019-09-10 19:48:07 +0100

    first draft of `roll_by()` function.

--- R/roll.R
+++ R/roll.R
@@ -1,11 +1,26 @@
-roll_forward <- function(schedule, to_following){
+roll_by <- function(schedule, n, unit, .p = NULL){
 
-  #, n = 1, .p = NULL
+  my_schedule <- schedule
 
-  date_test <- function(date){
+  if(class(unit) == "character"){
 
-    are_candidate_dates <- happen(to_following, date)
-  }
+    unit_freq <- lubridate::period(n, units = unit)
+
+    date_test <- function(date){
+
+      adjusted_date <- date %m-% unit_freq
+
+      result <- happen(my_schedule, adjusted_date)
+
+      if(!is.null(.p)){
+        is_applicable <- happen(.p, adjusted_date)
+        result <- result & is_applicable
+        }
+
+      result
+      }
+
+    }
 
   out <- list(date_test = date_test)
 
@@ -13,6 +28,15 @@ roll_forward <- function(schedule, to_following){
 
   class(out) <- "schedule"
 
+  if(!is.null(.p)){
+    applicable_schedule <- only_occur(my_schedule, dont_occur(.p))
+    out <- also_occur(out, applicable_schedule)
+  }
+
   out
 
 }
+
+
+
+
